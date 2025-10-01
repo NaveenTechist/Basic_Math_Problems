eaf40d29f0ee7f510bac784c39b005aeb6f47123
N A V E E N <162016882+NaveenTechist@users.noreply.github.com>
2025-10-01 16:48:42 +0530

    Sum of Digits Until Single Digit.cpp

--- Sum of Digits Until Single Digit.cpp
+++ Sum of Digits Until Single Digit.cpp
@@ -1,1 +1,11 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:
+    int sumOfDigits(int num) {
+       if(num == 0) return 0;
+        return  (num % 9 == 0) ? 9 : (num % 9);
+    }
+};
